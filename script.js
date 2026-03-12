57ce13cd220a7e8c56e95824efe67b64de0c8ba9
Obieb2024 <muhammadchobib07@gmail.com>
2026-03-12 18:08:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function bukaLink(tujuan, event) {
+    event.preventDefault(); 
+    let url = "";
+
+    if (tujuan === 'drive') {
+        url = "https://drive.google.com/drive/folders/MASUKKAN_LINK"; 
+    } else if (tujuan === 'gform') {
+        url = "https://forms.gle/jQBa676NR5vZuXLf9"; 
+    } else if (tujuan === 'admin1') {
+        url = "https://wa.me/6285692627679?text=Halo%20Admin%201,%20saya%20ingin%20bertanya%20seputar%20Oprec%20BEM.";
+    } else if (tujuan === 'admin2') {
+        url = "https://wa.me/6281804495792?text=Halo%20Admin%202,%20saya%20ingin%20bertanya%20seputar%20Oprec%20BEM.";
+    }
+
+    if (url !== "" && !url.includes("MASUKKAN_LINK")) {
+        window.open(url, '_blank');
+    } else {
+        alert("Tautan belum disetting panitia.");
+    }
+}
